11adb3efdc35cf9b178222ad0286157d06ad820a
Titus <dev@tituscoleman.com>
2023-05-18 20:26:12 -0400

    TF basics recap

--- demo-vpc-init/main.tf
+++ demo-vpc-init/main.tf
@@ -130,6 +130,31 @@ resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+  aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.generated.key_name
+
+  # specifies the user details for connecting directly to the server via SSH
+  # connection and provisioner blocks cannot standalone and must be within a corresponding resource block
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  // clones repo from hashicorp then invokes the setup script on the running EC2 instance
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
+
   tags = {
     Name  = local.server_name
     Owner = local.team
@@ -148,3 +173,125 @@ resource "aws_subnet" "variables-subnet" {
     Terraform = "true"
   }
 }
+
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+// NOT RECOMMENDED FOR PRODUCTION
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
+
+# Generates AWS keyPair from key file
+resource "aws_key_pair" "generated" {
+  key_name   = "MyAWSKey"
+  public_key = tls_private_key.generated.public_key_openssh
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
+
+# Create Security Groups - SSH
+resource "aws_security_group" "ingress-ssh" {
+  name   = "allow-all-ssh"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+  // Terraform removes the default rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create Security Group - Web Traffic
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web-${terraform.workspace}"
+  vpc_id      = aws_vpc.vpc.id
+  description = "Web Traffic"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "vpc-ping" {
+  name        = "vpc-ping"
+  vpc_id      = aws_vpc.vpc.id
+  description = "ICMP for Ping Access"
+  ingress {
+    description = "Allow ICMP Traffic"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+/* --------------------------------------------------- */
+# Terraform Resource Block - To Build Web Server in Public Subnet
+# resource "aws_instance" "web_server2" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+#   security_groups = [aws_security_group.vpc-ping.id,
+#   aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+#   associate_public_ip_address = true
+#   key_name                    = aws_key_pair.generated.key_name
+#   connection {
+#     user        = "ubuntu"
+#     private_key = tls_private_key.generated.private_key_pem
+#     host        = self.public_ip
+#   }
+#   # Leave the first part of the block unchanged and create our `local-exec` provisioner
+#   provisioner "local-exec" {
+#     command = "chmod 600 ${local_file.private_key_pem.filename}"
+#   }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo rm -rf /tmp",
+#       "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+#       "sudo sh /tmp/assets/setup-web.sh",
+#     ]
+#   }
+#   tags = {
+#     Name = "Web EC2 Server #2"
+#   }
+#   lifecycle {
+#     ignore_changes = [security_groups]
+#   }
+# }
