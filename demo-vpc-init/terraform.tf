3bda7b9ce3be3e00b0eec25fe843b5d3be8174a2
Titus <dev@tituscoleman.com>
2023-05-28 21:30:31 -0400

    s3 backend with dynamodb lock table

--- demo-vpc-init/terraform.tf
+++ demo-vpc-init/terraform.tf
@@ -3,5 +3,17 @@ terraform {
     bucket  = "terraform-state-tcoleman"
     profile = "default"
     region  = "us-east-1"
+
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
+
+  #   backend "remote" {
+  #     hostname     = "app.terraform.io"
+  #     organization = "tcoleman-org"
+  #     workspaces {
+  #       name = "test-aws-app"
+  #     }
+  #   }
 }
+
